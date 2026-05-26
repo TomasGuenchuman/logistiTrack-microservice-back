67f7cf092e6b1379710797c835a6aa6bc9a15a63
Tomas <tomasschool391@gmail.com>
2026-05-26 19:04:03 -0300

    Se agrego SWAGGER al proyecto

--- package-service/src/main.ts
+++ package-service/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración de las opciones de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Mi API de NestJS')
+    .setDescription('Descripción de los endpoints')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // ¡AQUÍ SE DEFINE LA RUTA!
+  // El primer argumento ('api') es el prefijo de la URL
+  SwaggerModule.setup('api', app, document);
 
   app.connectMicroservice({
     transport: Transport.REDIS,
