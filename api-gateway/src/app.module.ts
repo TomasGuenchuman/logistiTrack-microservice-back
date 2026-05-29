297e3bb9b63e15a4123e92f6dfe0187bc531b863
Tomas <tomasschool391@gmail.com>
2026-05-28 21:03:05 -0300

    Configuracion inicial y swagger

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PackageModule } from './package/package.module';
+import { VerificationModule } from './verification/verification.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PackageModule,
+    VerificationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
