dad5cb69bbfedb29f9bd6e10aaa7d5c11d6c81f7
Eliezer Sardinas <sardinaseliezer@gmail.com>
2026-06-01 12:42:26 -0300

    habilito cors para que las peticiones a la api no reboten

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // habilito los cors para no tener bloqueos de seguridad cruzada
+  app.enableCors({
+    origin: '*', // Permite que cualquier dispositivo se conecte (ideal para desarrollo local)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('LogistiTrack API Gateway')
     .setDescription('API Gateway documentation for LogistiTrack microservices')
@@ -15,6 +22,10 @@ async function bootstrap() {
 
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`API Gateway corriendo en el puerto: ${port}`);
+  console.log(`Documentación Swagger disponible en: http://localhost:${port}/api/docs`);
 }
 bootstrap();
