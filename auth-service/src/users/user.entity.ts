a4d2185058ea54d21b71a77f9cc71f62a9d2cd0e
Eliezer Sardinas <sardinaseliezer@gmail.com>
2026-05-31 23:46:44 -0300

    agrego los datos del usuario para el orm y sus servicios/modulos

--- auth-service/src/users/user.entity.ts
+++ auth-service/src/users/user.entity.ts
@@ -1,10 +1,30 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, } from 'typeorm';
 
 @Entity('users')
 export class User {
-  @PrimaryGeneratedColumn()
-  id!: number;
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    unique: true,
+  })
   email!: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  password!: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+  })
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+  })
+  updatedAt!: Date;
 }
---
+++ auth-service/src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findOneByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({
+      where: { email },
+    });
+  }
+}
