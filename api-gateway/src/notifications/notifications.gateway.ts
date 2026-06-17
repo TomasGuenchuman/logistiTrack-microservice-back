ab358b4b74060ac7be489aab41c2399c3c4ef14d
Eliezer Sardinas <sardinaseliezer@gmail.com>
2026-06-17 14:59:28 -0300

    simplifico NotificationsGateway, sin redis, map simple

--- api-gateway/src/notifications/notifications.gateway.ts
+++ api-gateway/src/notifications/notifications.gateway.ts
@@ -5,97 +5,59 @@ import {
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { Inject, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { Redis } from 'ioredis';
 import { JwtService } from '@nestjs/jwt';
 
+
 @WebSocketGateway({ cors: true })
-export class NotificationsGateway
-  implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit, OnModuleDestroy
+export class NotificationsGateway implements OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer()
   server!: Server;
 
-  private userSockets: Map<string, Map<string,string>> = new Map();
-
-  private redisSubscriber: Redis;
+  private userSockets: Map<string, string> = new Map();
 
   constructor(
-    @Inject('REDIS_CLIENT')
-    private readonly redisClient: Redis,
     private readonly jwtService: JwtService,
   ) {
-    /**
-     * Importante:
-     * No usamos directamente redisClient para subscribe,
-     * porque una conexión de Redis en modo subscriber queda dedicada a eso.
-     */
-    this.redisSubscriber = this.redisClient.duplicate();
-  }
-
-  async onModuleInit() {
-    this.redisSubscriber.on('error', (error) => {
-      console.error('Error en Redis subscriber:', error.message);
-    });
-
-    await this.redisSubscriber.subscribe('user_sessions');
-
-    console.log('API Gateway suscripto al canal Redis: user_sessions');
-
-    this.redisSubscriber.on('message', (channel, message) => {
-      if (channel === 'user_sessions') {
-        const data = JSON.parse(message);
-
-        if (data.action === 'FORCE_LOGOUT') {
-          this.sendForceLogout(data.userId,data.exceptDevice);
-        }
-      }
-    });
-  }
-
-  async onModuleDestroy() {
-    await this.redisSubscriber.quit();
   }
 
   handleConnection(client: Socket) {
     try {
       const token = client.handshake.auth.token;
       const payload = this.jwtService.verify(token);
+      console.log('B1 - handleConnection');
       const userId = payload.sub;
-      const deviceId = client.handshake.query.deviceId as string;
+      const existingDevices = this.userSockets.get(userId);
 
-      if (!this.userSockets.has(userId)) {
-        this.userSockets.set(userId, new Map());
+      console.log('B2 - userId: ', userId, '  sessionId: ', payload.sessionId);
+      if (existingDevices) {
+        this.sendForceLogout(userId);
       }
-      this.userSockets.get(userId)!.set(deviceId, client.id);
+      
+
+      this.userSockets.set(userId, client.id);
+
     } catch (error) {
       client.disconnect();
     }
   }
 
   handleDisconnect(client: Socket) {
-    for (const [userId, deviceSockets] of this.userSockets.entries()) {
-      for (const [deviceId, socketId] of deviceSockets.entries()) {
-        if (socketId === client.id) {
-          deviceSockets.delete(deviceId);
-          if (deviceSockets.size === 0) {
-            this.userSockets.delete(userId);
-          }
-          return;
-        }
+    for (const [userId, socketId] of this.userSockets.entries()) {
+      if (socketId === client.id) {
+        this.userSockets.delete(userId);
+        return;
       }
     }
   }
 
-  sendForceLogout(userId: string, exceptDevice?: string) {
-    const deviceSockets = this.userSockets.get(userId);
-      if (!deviceSockets) return;
+  sendForceLogout(userId: string) {
+    const socketId = this.userSockets.get(userId);
 
-      for (const [deviceId, socketId] of deviceSockets.entries()) {
-        if (deviceId === exceptDevice) continue;
-        this.server.to(socketId).emit('force_logout', {
-          message: 'Sesión cerrada por nuevo inicio de sesión'
-        });
-      }
+    if (!socketId) return;
+
+    this.server.to(socketId).emit('force_logout', {
+    message: 'Sesión cerrada por nuevo inicio de sesión'
+  });
   }
 }
